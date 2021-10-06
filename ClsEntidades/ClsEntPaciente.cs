644e11b157970ae1ede5362715ea39f54131b5d8
Anyi Vilaro <anmivi15@gmail.com>
2021-10-05 20:47:47 -0500

    Se crea aplicacion hospital y se crean clases para entidades y datos

---
+++ ClsEntidades/ClsEntPaciente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClsEntidades
+{
+    public class ClsEntPaciente
+    {
+        private string id_paciente;
+        private string nom_paciente;
+        private string dir_paciente;
+        private string tel_paciente;
+        private bool activo;
+        private bool tipo;
+
+        public string Id_paciente { get => id_paciente; set => id_paciente = value; }
+        public string Nom_paciente { get => nom_paciente; set => nom_paciente = value; }
+        public string Dir_paciente { get => dir_paciente; set => dir_paciente = value; }
+        public string Tel_paciente { get => tel_paciente; set => tel_paciente = value; }
+        public bool Activo { get => activo; set => activo = value; }
+        public bool Tipo { get => tipo; set => tipo = value; }
+    }
+}
